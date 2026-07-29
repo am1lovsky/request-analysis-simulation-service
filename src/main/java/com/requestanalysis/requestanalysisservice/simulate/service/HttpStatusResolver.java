6956e527bd4d12fa0334772f68230c0df41e52f3
am1lovsky <anatoliy.milovsky@gmail.com>
2026-07-29 20:11:51 +0200

    log added

--- src/main/java/com/requestanalysis/requestanalysisservice/simulate/service/HttpStatusResolver.java
+++ src/main/java/com/requestanalysis/requestanalysisservice/simulate/service/HttpStatusResolver.java
@@ -1,8 +1,10 @@
 package com.requestanalysis.requestanalysisservice.simulate.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class HttpStatusResolver {
 
@@ -10,6 +12,7 @@ public class HttpStatusResolver {
         try {
             return HttpStatus.valueOf(statusCode);
         } catch (IllegalArgumentException e) {
+            log.warn("Unrecognized HTTP status code: {}, falling back to 500", statusCode);
             return HttpStatus.INTERNAL_SERVER_ERROR;
         }
     }
